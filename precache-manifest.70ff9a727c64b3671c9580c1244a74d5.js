a6fa5807ebcfcdc459bdfe8bca6024a0414fcb04
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-03-12 18:11:57 +0100

    Updates

---
+++ precache-manifest.70ff9a727c64b3671c9580c1244a74d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f29b29a944535dbcf41efb04328a41",
+    "url": "/React-LightsOut/index.html"
+  },
+  {
+    "revision": "f0d205279f5c1fd69033",
+    "url": "/React-LightsOut/static/css/main.032cd1a1.chunk.css"
+  },
+  {
+    "revision": "80833e65b98e49e3812b",
+    "url": "/React-LightsOut/static/js/2.207c21a1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-LightsOut/static/js/2.207c21a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d205279f5c1fd69033",
+    "url": "/React-LightsOut/static/js/main.4fffe0b4.chunk.js"
+  },
+  {
+    "revision": "e9c428c5b73641df4d82",
+    "url": "/React-LightsOut/static/js/runtime-main.e3386b6d.js"
+  }
+]);
