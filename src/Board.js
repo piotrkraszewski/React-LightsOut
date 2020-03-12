4e7de1eb70ac88c71507d7bd59ea096c5a7079f4
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-03-12 18:19:28 +0100

    added lights out at the top

--- src/Board.js
+++ src/Board.js
@@ -91,11 +91,17 @@ class Board extends Component {
           <div className='neon-blue'>Out</div>
         </div>
         ) : (
-        <table className="Board">
-          <tbody>
-              {tblBoard}
-          </tbody>
-        </table>
+        <div>
+          <div className='Board-title'>
+            <div className='neon-orange'>Lights</div>
+            <div className='neon-blue'>Out</div>
+          </div>
+          <table className="Board">
+            <tbody>
+                {tblBoard}
+            </tbody>
+          </table>
+        </div>
         )}
       </div>
     )
